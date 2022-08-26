a2b36bcd462b1f31b4b2737fc2394a2d90fec20c
keijo@xvctz@tutanota.com <make@genchu.gentoo.org>
2022-08-26 09:54:03 +0300

    0

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
-#d verwsina
-VERSION = 0.1
+#d version
+VERSION = 1.0
 
 # Customize below to fit your system
 
@@ -11,8 +11,8 @@ X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
-#XINERAMALIBS  = -lXinerama
-#XINERAMAFLAGS = -DXINERAMA
+XINERAMALIBS  = -lXinerama
+XINERAMAFLAGS = -DXINERAMA
 
 # freetype
 FREETYPELIBS = -lfontconfig -lXft
--- d.c
+++ d.c
@@ -1759,7 +1759,7 @@ setup(void)
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	XChangeProperty(dpy, wmcheckwin, netatom[NetWMName], utf8string, 8,
-		PropModeReplace, (unsigned char *) " ", 1);
+		PropModeReplace, (unsigned char *) "0", 1);
 	XChangeProperty(dpy, root, netatom[NetWMCheck], XA_WINDOW, 32,
 		PropModeReplace, (unsigned char *) &wmcheckwin, 1);
 	/* EWMH support per view */
